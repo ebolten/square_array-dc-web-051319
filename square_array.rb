beb2b769f8437957eb843d94fe6fd28d761c92d5
Emily Bolten <emilybolten@gmail.com>
2019-05-07 18:54:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   # your code here
-  array.collect do |item|
+  array.each do |item|
     x = array.index(item)
     x.to_i
     array[x] = item ** 2
   end
-  return puts array
+  return array
 end
